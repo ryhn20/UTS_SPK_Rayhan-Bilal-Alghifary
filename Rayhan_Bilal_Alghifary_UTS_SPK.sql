2b2f0a08417e7f178ee372b3a432d0162b724e91
ryhn20 <138582957+ryhn20@users.noreply.github.com>
2023-10-31 17:59:41 +0700

    Add files via upload

---
+++ Rayhan_Bilal_Alghifary_UTS_SPK.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.rayhan_bilal_alghifary_spk (column1,column2,column3,column4,column5,column6,column7,column8,column9,column10) VALUES
+	 ('','Nama_TV','Layar','Resolution','Wifi','HDMI','Harga','Alternatif','Nilai_Akhir','Range'),
+	 ('','TCL 32S62','32 inch','1920 x 1080','Tidak',' 2 port','2,145,000','2.2372631','0.079097976','7'),
+	 ('','Mi TV 4A','32 inch','1366 x 768','Ya','3 port','2,199,000','2.4863698','0.087905094','5'),
+	 ('','TCL 32A3','32 inch','3840 x 2160','Ya','2 Port','2,099,000','1.9443658','0.068742651','10'),
+	 ('','CooCaa 32S5C','32 inch','3840 x 2160','Ya','2 Port','1,440,750','1.9443659','0.068742652','9'),
+	 ('','realme Smart TV','32 inch','1366 x 768','Ya','3 Port','1,985,000','2.0051304','0.070890973','8'),
+	 ('','LG 32LM630','32 inch','1366 x 768','Ya','2 Port','2,375,000','2.2986074','0.081266792','6'),
+	 ('','CooCaa 40S5','40 inch','3840 x 2160','Ya','2 Port','2,995,000','3.3888457','0.119811942','4'),
+	 ('','Samsung UA43 N5500','43 inch','1920 x 1080','Ya','1 Port','4,489,000','3.4846668','0.123199677','3'),
+	 ('','LG 43UK6300','43 inch','3840 x 2160','Ya','3 Port','4,925,000','4.6026355','0.162725231','1');
+INSERT INTO public.rayhan_bilal_alghifary_spk (column1,column2,column3,column4,column5,column6,column7,column8,column9,column10) VALUES
+	 ('','Polytron PLD 43AS1558','43 inch','1920 x 1080','Ya','2 Port','3,975,000','3.8924569','0.137617012','2');
